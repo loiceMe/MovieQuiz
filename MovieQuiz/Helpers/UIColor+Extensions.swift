d54467af27bd817fe1eee3d26942dfc315e20837
Dmitry Krivenko <d.n.krivenko@gmail.com>
2024-12-25 15:41:26 +0200

    Finish sprint 4

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var YPBlack: UIColor { UIColor.init(named: "YP Black") ?? UIColor.black }
+    static var YPWhite: UIColor { UIColor.init(named: "YP White") ?? UIColor.white }
+    static var YPGreen: UIColor { UIColor.init(named: "YP Green") ?? UIColor.green }
+    static var YPRed: UIColor { UIColor.init(named: "YP Red") ?? UIColor.red }
+    static var YPGray: UIColor { UIColor.init(named: "YP Gray") ?? UIColor.gray }
+    static var YPBackground: UIColor { UIColor.init(named: "YP Background") ?? UIColor.darkGray }
+}
